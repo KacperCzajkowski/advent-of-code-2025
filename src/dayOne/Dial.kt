71020a8878e3ea97f06bfed38229df1d0e927799
czajkowski.kacper99 <czajkowski.kacper99@gmail.com>
2025-12-02 22:05:28 +0100

    AoC: day 1

---
+++ src/dayOne/Dial.kt
@@ -0,0 +1,38 @@
+package dayOne
+
+class Dial(private var position: Int = 50, private var zerosCounter: Int = 0) {
+    fun processStep(direction: Char, units: Int) {
+        if (direction == 'L') {
+            turnLeft(units)
+        } else if (direction == 'R') {
+            turnRight(units)
+        }
+    }
+
+    fun getCountedZeros(): Int {
+        return zerosCounter
+    }
+
+    private fun turnLeft(units: Int) {
+        val actualUnit = units % 100
+        if (position > actualUnit) {
+            position -= actualUnit
+        } else {
+            position = (100 - (actualUnit - position)) % 100
+        }
+
+        if (position == 0) {
+            zerosCounter++
+        }
+    }
+
+    private fun turnRight(units: Int) {
+        val actualUnits = units % 100
+        position = (position + actualUnits) % 100
+
+
+        if (position == 0) {
+            zerosCounter++
+        }
+    }
+}
---
+++ src/dayOne/Main.kt
@@ -0,0 +1,20 @@
+package dayOne
+
+import java.io.File
+
+
+val TASK_2 = true
+
+fun main() {
+    val dial = Dial()
+    File("src/dayOne/input.txt")
+        .forEachLine {
+            val direction = it.first()
+            val units = it.drop(1).toInt()
+            when (TASK_2) {
+                true -> repeat (units) { dial.processStep(direction, 1) }
+                false -> dial.processStep(direction, units)
+            }
+        }
+    println("Zeros: ${dial.getCountedZeros()}")
+}
