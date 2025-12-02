e25e1fb8995ae489f603fff35fcd08398be1476c
czajkowski.kacper99 <czajkowski.kacper99@gmail.com>
2025-12-02 22:06:03 +0100

    code fix

--- src/dayOne/Main.kt
+++ src/dayOne/Main.kt
@@ -12,7 +12,7 @@ fun main() {
             val direction = it.first()
             val units = it.drop(1).toInt()
             when (TASK_2) {
-                true -> repeat (units) { dial.processStep(direction, 1) }
+                true -> repeat(units) { dial.processStep(direction, 1) }
                 false -> dial.processStep(direction, units)
             }
         }
