8e46ff28d859eb7c21bd993a19dac16dfa7f851e
czajkowski.kacper99 <czajkowski.kacper99@gmail.com>
2025-12-05 23:46:42 +0100

    AoC: day 3

---
+++ src/dayThree/Main.kt
@@ -0,0 +1,25 @@
+package dayThree
+
+import java.io.File
+
+const val PART_2 = true
+
+fun main() {
+    var sum: Long = 0
+    val extractor = MaxBatteryJoltageExtractor()
+    File("src/dayThree/input.txt")
+        .forEachLine {
+            val extract = resolve(extractor, it)
+            println("Found: $extract, whole: $it")
+            sum += extract
+        }
+    println("Value: $sum, Expected: 166345822896410, equal ${sum == 166345822896410}")
+}
+
+fun resolve(extractor: MaxBatteryJoltageExtractor, string: String): Long {
+    if (PART_2) {
+        return extractor.twelveBatteriesExtract(string)
+    }
+
+    return extractor.twoBatteriesExtract(string).toLong()
+}
---
+++ src/dayThree/MaxBatteryJoltageExtractor.kt
@@ -0,0 +1,49 @@
+package dayThree
+
+class MaxBatteryJoltageExtractor {
+    fun twoBatteriesExtract(bank: String): Int {
+        var firstBiggest = bank[0]
+        var secondBiggest = bank[1]
+
+        for (i in 2..<bank.length) {
+            if (toInt(firstBiggest, bank[i]) < toInt(secondBiggest, bank[i])) {
+                firstBiggest = secondBiggest
+                secondBiggest = bank[i]
+
+                continue
+            }
+
+            if (toInt(firstBiggest, secondBiggest) < toInt(firstBiggest, bank[i])) {
+                secondBiggest = bank[i]
+            }
+        }
+
+        return toInt(firstBiggest, secondBiggest)
+    }
+
+    fun twelveBatteriesExtract(bank: String): Long {
+        var result = StringBuilder()
+        var lastFoundOnIndex = 0
+
+        repeat(12) { i ->
+            val startIndex = if (i == 0) 0 else lastFoundOnIndex + 1
+            var currentBiggest = bank[startIndex].digitToInt()
+            lastFoundOnIndex = startIndex
+            val max = bank.length - 12 + i
+            for (j in startIndex + 1..max) {
+                val possibleValue = bank[j].digitToInt()
+                if (currentBiggest < possibleValue) {
+                    currentBiggest = possibleValue
+                    lastFoundOnIndex = j
+                }
+            }
+            result = result.append(currentBiggest)
+        }
+
+        return result.toString().toLong()
+    }
+
+    private fun toInt(first: Char, second: Char): Int {
+        return first.plus(second.toString()).toInt()
+    }
+}
